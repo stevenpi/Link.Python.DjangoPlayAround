3f45197eac4f1b4b339b1a2e318c3758f688d55b
Steven Pilatschek <steven.pilatschek@gmail.com>
2018-03-09 10:59:59 +0100

    force push on heroku 

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+# comment to force push on heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
